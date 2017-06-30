8d823f536c132570743ea9e1668c487dd8b68507
yoshow <ruanyu@live.com>
2017-06-30 14:54:45 +0800

    include file

--- example/Home.vue
+++ example/Home.vue
@@ -1,22 +1,24 @@
 <template>
-    <div>
-        <div>this is Header</div>
-        <div v-bind="model1">{{text}}</div>
-    </div>
+  <div>
+    <div>this is Header</div>
+    <div v-bind="model1">{{text}}</div>
+  </div>
 </template>
 <style>
 #header {
-    width: 100px;
+  width: 100px;
 }
 </style>
 <script>
 export default {
-    data() {
-        return {
-            msg: 'hello vue'
-        }
-    },
-    components: {}
+  // 定义模板变量
+  // template,
+  data() {
+    return {
+      msg: 'hello vue'
+    }
+  },
+  components: {}
 }
 </script>
 
